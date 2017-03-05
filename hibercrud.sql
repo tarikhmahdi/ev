42ef8c7f7e938ba91340c1fc1f560a602276eab6
TARIKH <TARIKH@DESKTOP-QHPNNBV>
2017-03-05 10:46:01 +0600

    first commit

---
+++ hibercrud.sql
@@ -0,0 +1,63 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.0.41-community-nt
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema hibercrud
+--
+
+CREATE DATABASE IF NOT EXISTS hibercrud;
+USE hibercrud;
+
+--
+-- Definition of table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(45) NOT NULL,
+  `pass` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `sex` varchar(45) NOT NULL,
+  `age` int(10) unsigned NOT NULL,
+  `dob` date NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`,`name`,`pass`,`email`,`sex`,`age`,`dob`) VALUES 
+ (1,'avi','1234','aa@aa.com','Male',25,'2017-03-08'),
+ (2,'asdf','1234','aa@aa.com','Female',24,'2017-01-12'),
+ (4,'ASDF','1234','aa@aa.com','Female',24,'2017-01-12'),
+ (5,'ASDF','1234','aa@aa.com','Female',24,'2017-01-12'),
+ (6,'ASDF','1234','aa@aa.com','Female',24,'2017-01-12'),
+ (7,'ASDF','1234','aa@aa.com','Female',24,'2017-01-12');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
